2bf0195cbd6f913c2985bf53f3d6c0e2c3fe070f
Justin Herrick <justin@justinherrick.com>
2020-12-03 14:11:56 -0600

    Refactor day 3

--- 03/main.rb
+++ 03/main.rb
@@ -1,37 +1,26 @@
 require_relative "../helpers"
 
-puzzle "3.1", mode: :count, answer: 191 do |input, count|
-  rotation = input.first.length - 1
-  left = 3
-  down = 1
-  pos = {x: 0, y: 0}
-  input.length.times do |n|
-    pos[:x] = (pos[:x] + left) % rotation
-    pos[:y] = (pos[:y] + down) % input.length
-    terrain = input[pos[:y]][pos[:x]]
-    count += 1 if terrain == "#"
-    puts "[#{"%4d" % count}][#{"%4d" % n}][#{"%4d" % pos[:x]},#{"%4d" % pos[:y]}] | " + input[pos[:y]].chomp + " | #{terrain}"
-  end
-  puts count
-end
-
 def count_slope(input, left, down)
-  puts "----------------------------------------"
+  rotation = input.first.length - 1
   count = 0
   pos = {x: 0, y: 0}
-  puts ["count", 0, "left", left, "down", down, "pos", pos].map(&:to_s).join(": ")
-  rotation = input.first.length - 1
   input.length.times do |n|
     break if input[pos[:y]].nil?
+
     terrain = input[pos[:y]][pos[:x]]
     count += 1 if terrain == "#"
+
     input[pos[:y]][pos[:x]] = "\033[31m#{input[pos[:y]][pos[:x]]}\033[0m"
-    puts "[#{"%4d" % count}][#{"%4d" % n}][#{"%4d" % pos[:x]},#{"%4d" % pos[:y]}] | " + input[pos[:y]].chomp + " | #{terrain}"
+    puts "[#{"%4d" % count}][#{"%4d" % n}][#{"%4d" % pos[:x]},#{"%4d" % pos[:y]}] | " + input[pos[:y]].chomp + " | #{"\033[31m#{terrain}\033[0m" if terrain == "#"}"
 
     pos[:x] = (pos[:x] + left) % rotation
     pos[:y] = (pos[:y] + down)
   end
   count
+end
+
+puzzle "3.1", mode: :count, answer: 191 do |input, count|
+  count_slope(input, 3, 1)
 end
 
 puzzle "3.2" do |input|
