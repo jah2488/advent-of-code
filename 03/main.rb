40ef8a88cfa699cabd44b031d1d995532f032d6c
Justin Herrick <justin@justinherrick.com>
2020-12-03 14:06:14 -0600

    finishing 3.2

--- 03/main.rb
+++ 03/main.rb
@@ -14,3 +14,36 @@ puzzle "3.1", mode: :count, answer: 191 do |input, count|
   end
   puts count
 end
+
+def count_slope(input, left, down)
+  puts "----------------------------------------"
+  count = 0
+  pos = {x: 0, y: 0}
+  puts ["count", 0, "left", left, "down", down, "pos", pos].map(&:to_s).join(": ")
+  rotation = input.first.length - 1
+  input.length.times do |n|
+    break if input[pos[:y]].nil?
+    terrain = input[pos[:y]][pos[:x]]
+    count += 1 if terrain == "#"
+    input[pos[:y]][pos[:x]] = "\033[31m#{input[pos[:y]][pos[:x]]}\033[0m"
+    puts "[#{"%4d" % count}][#{"%4d" % n}][#{"%4d" % pos[:x]},#{"%4d" % pos[:y]}] | " + input[pos[:y]].chomp + " | #{terrain}"
+
+    pos[:x] = (pos[:x] + left) % rotation
+    pos[:y] = (pos[:y] + down)
+  end
+  count
+end
+
+puzzle "3.2" do |input|
+  slopes = [
+    [1, 1], [3, 1], [5, 1], [7, 1], [1, 2]
+  ]
+
+  dup = ->(m) { m.map(&:dup).dup }
+
+  slopes
+    .map { |(left, down)| count_slope(dup.call(input), left, down) }
+    .tap { |x| puts x }
+    .reduce(:*)
+    .tap { |n| puts n }
+end
