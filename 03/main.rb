46c2fc8aca487d43975ff39f228c035d290bf4b3
Justin Herrick <justin@justinherrick.com>
2020-12-03 12:43:53 -0600

    Re-reading the instructions is helpful.

--- 03/main.rb
+++ 03/main.rb
@@ -1,6 +1,6 @@
 require_relative "../helpers"
 
-puzzle "3.1", mode: :count do |input, count|
+puzzle "3.1", mode: :count, answer: 191 do |input, count|
   rotation = input.first.length - 1
   left = 3
   down = 1
@@ -9,10 +9,7 @@ puzzle "3.1", mode: :count do |input, count|
     pos[:x] = (pos[:x] + left) % rotation
     pos[:y] = (pos[:y] + down) % input.length
     terrain = input[pos[:y]][pos[:x]]
-    if terrain == "#"
-      count += 1
-    end
-
+    count += 1 if terrain == "#"
     puts "[#{"%4d" % count}][#{"%4d" % n}][#{"%4d" % pos[:x]},#{"%4d" % pos[:y]}] | " + input[pos[:y]].chomp + " | #{terrain}"
   end
   puts count
